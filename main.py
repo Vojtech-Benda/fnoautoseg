ef82d39b0c4bd485e55219365012bb7b9bf3eae9
Vojtěch-Benda <vojtech.benda@atlas.cz>
2025-04-09 13:04:09 +0200

    updated watchdog

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import time
 import json
 import subprocess
 import shutil
-import keyboard
+# import keyboard
+from pynput import keyboard
 import threading
 
 from watchdog.observers import Observer
@@ -13,69 +14,55 @@ from watchdog.events import FileSystemEventHandler
 INPUT_DIR_PATH = "./input"
 CONFIG_PATH = "./config.json"
 PROCESS_PATH = None
+CHECK_INTERVAL = 20
 
 def reload_config():
     print("config updated")
     return True
 
 
-class ConfigHandler(FileSystemEventHandler):
-    def on_modified(self, event):
-        if event.src_path.endswith("config.json"):
-            code = reload_config()
-            if code:
-                print("[WATCHDOG] Config update success")
-            else:
-                print("[WATCHDOG] Config update failed")
-
-if len(sys.argv) == 2:
-    INPUT_DIR_PATH = sys.argv[1]
-
-last_config_time = 0
-model_name = None
-running = True
-
-def esc_listener():
-    global running
-    keyboard.wait('esc')
-    print("\n[WATCHER] ESC pressed, exiting...")
-    running = False
-
-listener_thread = threading.Thread(target=esc_listener, daemon=True)
-listener_thread.start()
-
-def load_config():
-    global model_name, last_config_time
-    if os.path.exists(CONFIG_PATH):
-        mod_time = os.path.getmtime(CONFIG_PATH)
-        if mod_time != last_config_time:
+class Config(FileSystemEventHandler):
+    def __init__(self):
+        self.process = None
+        self.model = None
+        
+    def load_config(self):
+        if os.path.exists(CONFIG_PATH):
             with open(CONFIG_PATH) as file:
                 config_file = json.load(file)
-                model_name = config_file["model"]
-                print(f"[CONFIG] Using model: {model_name}")
-            last_config_time = mod_time
+                self.process = config_file["process"]
+                self.unet_name = config_file["unet_name"]
+                return True
+            return False
+    
+    def on_modified(self, event):
+        if event.src_path.endswith("config.json"):
+            if self.load_config():
+                print("[WATCHER] Config modified")
+                return
+            print("[WATCHER] Config modify failed")
+            return
+ 
+
+def esc_listener(key):
+    global running
+    if key == keyboard.Key.esc:
+        print("\n[WATCHER] ESC pressed, exiting...")
+        running = False
+
+def start_listener():
+    listener = keyboard.Listener(on_press=esc_listener)
+    listener.start()
+    return listener
             
-def get_folder_size():
-    total = 0
+def get_file_count():
     files = os.listdir(INPUT_DIR_PATH)
-    total = sum(os.path.getsize(f) for f in files if os.path.isfile(f))
-        # fp = os.path.join(INPUT_DIR_PATH, f)
-    return total, len(files)
+    dir_size = sum(os.path.getsize(f) for f in files if os.path.isfile(f))
+    return len(files), dir_size
 
-def is_transfer_done(dir_path, wait_time=5, retries=3):
-    for _ in range(retries):
-        last_size, _ = get_folder_size()
-        time.sleep(wait_time)
-        current_size, files_count = get_folder_size()
-        if current_size != last_size:
-            continue
-        else:
-            return True, files_count
-    return False, 0
-
-def process_data():
-    print(f"[PROCESSING] Running process ")
-    process = subprocess.run(["py", "dcm2mha.py"])
+def process_data(config: Config):
+    print(f"[{config.process}] Running process ")
+    process = subprocess.run(["py", f"{config.process}.py"])
     if process.returncode == -1:
         print(f"[FAIL] Process failed ")
         return False
@@ -85,30 +72,53 @@ def process_data():
     return False
     
 def main():
+    running = True
+    
     print("[WATCHER] Starting DICOM watcher...")
     print("[WATCHER] Press ESC to exit")
+    start_listener()
+    
     observer = Observer()
-    observer.schedule(ConfigHandler(), path=".", recursive=False)
+    config = Config()
+    config.load_config()
+    
+    observer.schedule(config, path=".", recursive=False)
     observer.start()
     
+    check_value = 0
+    last_dir_size = 0    
     while running:
-        load_config()
-        transfer_done, files_count = is_transfer_done(INPUT_DIR_PATH)
-        if transfer_done and files_count > 0:
-            print(f"[WATCHER] Found {files_count} files")
-            process_success = process_data()
+        
+        if check_value % 5 == 0:
+            print("[WATCHER] Waiting...")
+        
+        if check_value >= CHECK_INTERVAL:
+            file_count, current_dir_size = get_file_count()
+            if last_dir_size != current_dir_size:
+                print("[WATCHER] No files to process")
+                continue
+            
+            print(f"[WATCHER] Found {file_count} files")
+            last_dir_size = current_dir_size
+            
+            print(f"[{config.process}] Processing data")
+            process_success = process_data(config)
             if process_success:
-                print(f"[WATCHER] Processed {files_count} files")
-                files = os.listdir(INPUT_DIR_PATH)
-                for f in files:
-                    fp = os.path.join(INPUT_DIR_PATH, f)
-                    # shutil.move(fp, "./processed")
-                print(f"[WATCHER] Moved {files_count} to ./processed")
-        else:
-            print("[WATCHER] No files to process")
-        # time.sleep(2)
+                print("[WATCHER] Moving files to ./processed")
+                move_files()
+            check_value = 0
+        time.sleep(1)
+        check_value += 1
     observer.stop()
     print("[WATCHER] Exiting program")
+
+def move_files():
+    files = os.listdir(INPUT_DIR_PATH)
+    print(f"[WATCHER] Processed {len(files)} files")
+    for f in files:
+        fp = os.path.join(INPUT_DIR_PATH, f)
+        shutil.move(fp, "./processed")
+    print(f"[WATCHER] Moved {len(files)} to ./processed")
     
 if __name__ == "__main__":
     main()
